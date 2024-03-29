1a267fe2c591c5f68f7210fc464978b1e1f1d574
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-03-29 17:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const questions = [
     options: ["3", "4", "5", "6"],
     correctAnswer: "4"
   },
-  {
+  { 
     question: "What is the capital of France?",
     options: ["London", "Paris", "Berlin", "Rome"],
     correctAnswer: "Paris"
