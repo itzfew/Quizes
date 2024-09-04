a8a6ebbbbf90f6af53d31f5cb5c7683365270e70
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-09-04 23:45:28 +0530

    Update login_script.js

--- login_script.js
+++ login_script.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAolcB_o6f1CQPbLSYrMKTYaz_xYs54khY",
-  authDomain: "quizapp-1ae20.firebaseapp.com",
-  projectId: "quizapp-1ae20",
-  storageBucket: "quizapp-1ae20.appspot.com",
-  messagingSenderId: "626886802317",
-  appId: "1:626886802317:web:df08c307697ca235c45bc4",
-  measurementId: "G-NKJTC5C1XW"
+  apiKey: "AIzaSyAJVzWcSVu7nW-069bext5W6Nizx4sfxIA",
+  authDomain: "edu-hub-c81b5.firebaseapp.com",
+  projectId: "edu-hub-c81b5",
+  storageBucket: "edu-hub-c81b5.appspot.com",
+  messagingSenderId: "560742513136",
+  appId: "1:560742513136:web:102edd272982704fdb8535",
+  measurementId: "G-78TC8XTPF7"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
